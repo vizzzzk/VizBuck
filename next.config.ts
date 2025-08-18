50d5ace90566fd65a385693955e972d295e6b785
Incoming Virus <Incomingvirus.IV@gmail.com>
2025-08-18 14:10:37 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,11 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+    ],
+  },
+   devIndicators: {
+    allowedDevOrigins: [
+        'https://*.cloudworkstations.dev',
     ],
   },
 };
